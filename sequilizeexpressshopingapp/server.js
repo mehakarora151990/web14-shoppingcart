bb60122fd8bb86654e221d25439ea271e00c7682
DEEPAK MALIK <deepakm661@gmail.com>
2019-04-05 22:53:06 +0530

    mehak corrected

---
+++ sequilizeexpressshopingapp/server.js
@@ -0,0 +1,15 @@
+
+const express = require('express')
+
+const path = require('path')
+const app = express();
+app.use(express.json())
+
+app.use(express.urlencoded({extended:true}))
+
+
+
+app.use('/',express.static(path.join(__dirname,'public')))
+app.use('/api',require('./routes/api').route)
+
+app.listen(2567,()=> console.log('server started at http://localhost:2567'))
